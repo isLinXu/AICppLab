f94a3a5d592f8a8f352aa3451803076235ee301c
isLinXu <2267379130@qq.com>
2022-05-06 11:09:20 +0800

    1.add global.h

---
+++ global.h
@@ -0,0 +1,14 @@
+//
+// Created by linxu on 2022/5/6.
+//
+
+#ifndef AICPPLAB_GLOBAL_H
+#define AICPPLAB_GLOBAL_H
+
+#include "opencv4/opencv2/opencv.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+#endif //AICPPLAB_GLOBAL_H
