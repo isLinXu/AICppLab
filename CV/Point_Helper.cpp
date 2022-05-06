197ac2c0c4520313ff4e9264b1b55f9bc0fa64c3
isLinXu <2267379130@qq.com>
2022-05-06 11:09:50 +0800

    1.add Point_Helper.cpp and .h

--- CV/Point_Helper.cpp
+++ CV/Point_Helper.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Point_Helper.h"
+
+int PointHelper::testPoint() {
+    Point pt1;
+    pt1.x = 10;
+    pt1.y = 8;
+
+    Point pt2 = Point(10, 8);
+    cout << "pt1=>" << pt1 << "pt2=>" << pt2 << endl;
+}
--- CV/Point_Helper.h
+++ CV/Point_Helper.h
@@ -5,4 +5,15 @@
 #ifndef AICPPLAB_POINT_HELPER_H
 #define AICPPLAB_POINT_HELPER_H
 
+#include "opencv4/opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+class PointHelper {
+
+    int testPoint();
+};
+
 #endif //AICPPLAB_POINT_HELPER_H
